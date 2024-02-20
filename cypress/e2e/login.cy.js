9523b6478feeb39aac3571a3bb1be88875b4a42b
Madhuri Ikhankar <madhuri@imriel.com>
2024-02-20 15:26:44 +0530

    login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,5 @@
+describe('The Home Page', () => {
+    it('successfully loads', () => {
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') // change URL to match your dev URL
+    })
+  })

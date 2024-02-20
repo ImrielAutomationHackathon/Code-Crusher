3db6f102200a65fd66cd5a9fd1061c9a33467e28
PoojaSarda123 <105299282+PoojaSarda123@users.noreply.github.com>
2024-02-20 15:07:07 +0530

    changes2

---
+++ cypress/e2e/Applitools/visual-tests.cy.js
@@ -0,0 +1,15 @@
+  /// <reference types="@applitools/eyes-cypress" />
+describe('Hello world', () => {
+  it('works', () => {
+    cy.visit('https://applitools.com/helloworld');
+    cy.eyesOpen({
+      appName: 'Hello World!',
+      testName: 'My first JavaScript test!',
+      browser: { width: 800, height: 600 },
+    });
+    cy.eyesCheckWindow('Main Page');
+    cy.get('button').click();
+    cy.eyesCheckWindow('Click!');
+    cy.eyesClose();
+  });
+});

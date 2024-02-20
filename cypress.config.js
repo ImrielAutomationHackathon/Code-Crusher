05cae579a07f4df3262b72d5ee692f543915e418
PoojaSarda123 <105299282+PoojaSarda123@users.noreply.github.com>
2024-02-20 17:39:10 +0530

    unused examples

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,9 @@
-// const { defineConfig } = require('cypress')
-// const eyesPlugin = require('@applitools/eyes-cypress')
-// module.exports = eyesPlugin(defineConfig({
-//   // the e2e or component configuration
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//     }
-//   }
-// }))
-
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 const eyesPlugin = require('@applitools/eyes-cypress')
-module.exports = defineConfig({
+module.exports = eyesPlugin(defineConfig({
+  // the e2e or component configuration
   e2e: {
-    supportFile: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+    }
+  }
+}))

b9454439c39589808889e6d6c4bb43af3a6c6edd
PoojaSarda123 <105299282+PoojaSarda123@users.noreply.github.com>
2024-02-21 16:59:36 +0530

    Added successfully login code audio

--- cypress/e2e/LoginOrangeHrm.cy.js
+++ cypress/e2e/LoginOrangeHrm.cy.js
@@ -1,10 +1,13 @@
 it('Logging into Orange Hrm', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    var userName = cy.xpath('//input[@name="username"]').click().type('Adm!n')
-  
-    var userPassword = cy.xpath('//input[@name="password"]').click().type('admin1234')
+    var invalidUserName = cy.xpath('//input[@name="username"]').click().type('Adm!n')
+    var invalidUserPassword = cy.xpath('//input[@name="password"]').click().type('admin1234')
+
+    var userName = cy.xpath('//input[@name="username"]').click().type('Admin')
+    var userPassword = cy.xpath('//input[@name="password"]').click().type('admin123')
+    
     cy.xpath('//button[@type="submit"]').click()
-    if(userName !== 'Admin' || userPassword !== 'admin123')
+    if(invalidUserName !== 'Admin' || invalidUserPassword !== 'admin123')
     {
       describe('Play Music', () => {
         it('Lost', () => {
@@ -16,5 +19,19 @@ it('Logging into Orange Hrm', () => {
               })
         })
     })
+    }
+    else if(userName === 'Admin' || userPassword === 'admin123'){
+      {
+        describe('Play Music', () => {
+          it('Lost', () => {
+              cy.readFile('cypress/audio/Lost.mp3', 'base64').then((mp3) => {
+                  const uri = 'data:audio/mp3;base64,' + mp3
+                  const audio = new Audio(uri)
+                
+                  audio.play()
+                })
+          })
+      })
+      }
     }
   });
